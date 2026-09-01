131e6c1bb193836f2af1548e7a731cfc76b84a50
liuweitao <tao@sadais.com>
2026-09-01 12:56:34 +0800

    🔧 chore(plugins): 更新归档会话插件

--- install.sh
+++ install.sh
@@ -29,7 +29,7 @@ readonly PLUGIN_SOURCES=(
   "@linxin666/dsh-web-all@0.3.9"
   "${BETTER_SIDEBAR_FORK}"
   "github:lwt-sadais/dsh-at-file#6dbc6209a881c97ae094081e5fb8899a9f4b1b05"
-  "github:lwt-sadais/dsh-archived-sessions#ada246b0def6db8fc6cdeb424abb520e56ccd068"
+  "github:lwt-sadais/dsh-archived-sessions#6154d11f11493cc24057321a69fbef4bc346f05b"
   "github:lwt-sadais/dsh-git-diff#69c8458d3eefc507f4512983934cc046b4e736dd"
   "github:lwt-sadais/dsh-git-history#cf22d3e2c839d38f63064568021cdc2b854dd41d"
   "github:lwt-sadais/dsh-local-file-reference#4ccc956cc14b1e2d4c19634287b52dcfc3a3c955"
